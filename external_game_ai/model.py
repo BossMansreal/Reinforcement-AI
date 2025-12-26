91da1e0f1e6c51918934108bce968f9dee7696ae
BossMan <bmyers373@student.bakercharters.org>
2025-12-26 15:16:08 -0800

    Create model.py

---
+++ external_game_ai/model.py
@@ -0,0 +1,19 @@
+import torch
+import torch.nn as nn
+
+class MinecraftPolicyNet(nn.Module):
+    def __init__(self, state_dim, action_dim):
+        super().__init__()
+
+        self.net = nn.Sequential(
+            nn.Linear(state_dim, 256),
+            nn.ReLU(),
+
+            nn.Linear(256, 256),
+            nn.ReLU(),
+
+            nn.Linear(256, action_dim)
+        )
+
+    def forward(self, x):
+        return self.net(x)
